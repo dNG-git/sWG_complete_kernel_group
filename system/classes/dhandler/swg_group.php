870674f233b95c565ae2fb22ec3617b6f864a7c5
@4u <>
2009-07-10 10:06:41 +0200

    Change: Removed "$Id$" keyword because we do no longer support it.

--- system/classes/dhandler/swg_group.php
+++ system/classes/dhandler/swg_group.php
@@ -28,7 +28,6 @@ this program; if not, write to the Free Software Foundation, Inc.,
 ----------------------------------------------------------------------------
 http://www.direct-netware.de/redirect.php?licenses;gpl
 ----------------------------------------------------------------------------
-$Id$
 #echo(sWGkernelGroupVersion)#
 sWG/#echo(__FILEPATH__)#
 ----------------------------------------------------------------------------
